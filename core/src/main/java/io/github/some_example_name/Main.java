c92366a31c87f3872c6adc976805ce09615006e3
Tom Hitchcock <thehitch1000@users.noreply.github.com>
2025-08-08 00:30:47 +0100

    Initial Commit

---
+++ core/src/main/java/io/github/some_example_name/Main.java
@@ -0,0 +1,25 @@
+package io.github.some_example_name;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends ApplicationAdapter {
+
+
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+
+
+    }
+}
