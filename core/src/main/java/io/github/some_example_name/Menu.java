523dd1484bdbbb4fba3c90b8bb02004e253a4e2c
Tom <git config user.email "thehitch1000@users.noreply.github.com>
2025-09-02 17:32:43 +0100

    commit

--- core/src/main/java/io/github/some_example_name/Menu.java
+++ core/src/main/java/io/github/some_example_name/Menu.java
@@ -13,8 +13,53 @@ import java.util.List;
 import static com.badlogic.gdx.Gdx.input;
 
 public class Menu {
+    private boolean open;
+    private float xTitle, yTitle, xCentre, yCentre;
     List<Button> buttons;
+    String title;
 
+
+    public Menu(String title, float xCentre, float yCentre, BitmapFont font) {
+        buttons = null;
+        this.title = title;
+        this.open = false;
+        this.xTitle = 0;
+        this.yTitle = 0;
+
+        this.xCentre = 0;
+        this.yCentre = 0;
+
+        setTitle(font);
+    }
+
+    public void setTitle(BitmapFont font) {
+        GlyphLayout layout = new GlyphLayout();
+        layout.setText(font, title);
+
+        xTitle = xCentre - layout.width / 2;
+        yTitle = yCentre - layout.height / 2;
+    }
+
+
+    public void AddButton(Button button) {
+        buttons.add(button);
+    }
+
+    public void Draw(ShapeRenderer sr, SpriteBatch batch, BitmapFont font) {
+        if (open) {
+            buttons.forEach(button -> button.Draw(sr, batch, font));
+            font.draw(batch, title, xTitle, yTitle);
+        }
+    }
+    public void CheckClick() {
+        buttons.forEach(button -> button.CheckClick());
+    }
+    public void Open() {
+        open = true;
+    }
+    public void Close() {
+        open = false;
+    }
 
 }
 
@@ -23,14 +68,18 @@ class Button {
     private Runnable action;
     private Rect buttonShape;
     private float xText, yText;
+    private float centreX, centreY;
 
-    public Button(String text, Runnable action, int x, int y, int width, int height) {
+    public Button(String text, Runnable action, float centreX, float centreY) {
         this.text = text;
         this.action = action;
-        this.buttonShape = new Rect(x, y, width, height, new Color(0.42f, 0.42f, 0.43f, 0.25f));
+        this.buttonShape = null;
 
         this.xText = 0;
         this.yText = 0;
+
+        this.centreX = centreX;
+        this.centreY = centreY;
     }
     public void CheckClick() {
         if (buttonShape.isPointInShape(new FloatPoint(Gdx.input.getX(), GameData.getInstance().getScreenHeight() - Gdx.input.getY()))) {
@@ -40,9 +89,14 @@ class Button {
         }
     }
 
-    public void FindTextPosition() {
+    public void FindTextPosition(BitmapFont font) {
         GlyphLayout layout = new GlyphLayout();
         layout.setText(font, text);
+
+        xText = centreX - layout.width / 2;
+        yText = centreY - layout.height / 2;
+
+        buttonShape = new Rect(xText - 10, yText - 10, layout.width + 20, layout.height + 20, new Color(0.42f, 0.42f, 0.43f, 0.25f));
     }
 
     public void DrawButton(ShapeRenderer sr) {
